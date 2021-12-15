8262a84099bf7b97ba54b551010844ef43fbd59c
Aaron K <tonberryspawn@gmail.com>
2021-12-15 00:04:22 -0800

    tests passed

--- updateSpots.js
+++ updateSpots.js
@@ -8,9 +8,25 @@
  */
 
 const updateSpots = function (state, appointments, id) {
+  const daysOfWeek = {Monday:0, Tuesday:1, Wednesday:2, Thursday:3, Friday:4};
+  const dayIndex = daysOfWeek[state.day];
+  
+  const days = state.days.map(day => {return {...day}});
+
+  // console.log('___appointments[id]', appointments[id]);
+  // console.log('___state.appointments[id]', state.appointments[id]);
+  const prevState = state.appointments[id].interview;
+  const newState = appointments[id].interview;
+
+  if (!prevState && newState) { // create
+    days[dayIndex].spots--;
+  }
+  if (prevState && !newState) { // delete
+    days[dayIndex].spots++;
+  }
 
   // return days array
-  return [];
+  return days;
 };
 
 module.exports = updateSpots;
